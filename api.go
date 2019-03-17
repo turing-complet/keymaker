6d22dfd05fc2765c51c669d75768f6b0d45947da
jon <jhagg314@gmail.com>
2019-03-17 00:36:25 -0700

    aes encrypt

--- api.go
+++ api.go
@@ -9,15 +9,37 @@ import (
 	"crypto/sha512"
 	"encoding/hex"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
 	"net/http"
+	"os/exec"
 	"strconv"
+	"strings"
 
 	"github.com/gorilla/mux"
 )
 
+var symmetricKeys = make(map[string][]byte)
+
+type symmetricKey struct {
+	ID  string
+	Key []byte
+}
+
 func index(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode("Welcome to the secret cryptographic service 😎")
+}
+
+func getUUID(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(newUUID())
+}
+
+func newUUID() string {
+	uuid, _ := exec.Command("uuidgen").Output()
+	// fmt.Printf("%s", uuid)
+	return strings.TrimSuffix(string(uuid), "\n")
+
 }
 
 func sha256Api(w http.ResponseWriter, r *http.Request) {
@@ -47,26 +69,55 @@ func createSymmKey(w http.ResponseWriter, r *http.Request) {
 			json.NewEncoder(w).Encode("Please specity bits as query string")
 			return
 		}
-		key := make([]byte, bits)
+		key := make([]byte, bits/8)
 		_, err = rand.Read(key)
 		if err != nil {
 			json.NewEncoder(w).Encode(err)
 		} else {
-			json.NewEncoder(w).Encode(key)
+			keyid := newUUID()
+			symmetricKeys[keyid] = key
+			resp := &symmetricKey{
+				ID:  keyid,
+				Key: key,
+			}
+			json.NewEncoder(w).Encode(resp)
 		}
 	}
 }
 
+// TODO: omit actual keys, persistence..
+func listSymmKeys(w http.ResponseWriter, r *http.Request) {
+	json.NewEncoder(w).Encode(symmetricKeys)
+}
+
 func aesEncrypt(w http.ResponseWriter, r *http.Request) {
 	params := mux.Vars(r)
-	key := []byte(params["key"])
+	keyid := params["keyid"]
 	plaintext := []byte(params["plaintext"])
-	nonce := make([]byte, 12)
+	key, exists := symmetricKeys[keyid]
+	if !exists {
+		json.NewEncoder(w).Encode("Key not found.")
+	}
 	block, _ := aes.NewCipher(key)
-	aesgcm, _ := cipher.NewGCM(block)
-	ciphertext := aesgcm.Seal(nil, nonce, plaintext, nil)
+	gcm, _ := cipher.NewGCM(block)
+	nonce := make([]byte, gcm.NonceSize())
+	io.ReadFull(rand.Reader, nonce)
+	ciphertext := gcm.Seal(nonce, nonce, plaintext, nil)
 	json.NewEncoder(w).Encode(ciphertext)
+}
 
+func aesDecrypt(w http.ResponseWriter, r *http.Request) {
+	fmt.Println("doing decryption")
+	params := mux.Vars(r)
+	keyid := params["keyid"]
+	fmt.Printf("ciphertext: %v", params["ciphertext"])
+	ciphertext := []byte(params["ciphertext"])
+	fmt.Printf("ciphertext_bytes: %v", ciphertext)
+	key := symmetricKeys[keyid]
+	block, _ := aes.NewCipher(key)
+	gcm, _ := cipher.NewGCM(block)
+	plaintext, _ := gcm.Open(nil, ciphertext[:gcm.NonceSize()], ciphertext[gcm.NonceSize():], nil)
+	json.NewEncoder(w).Encode(plaintext)
 }
 
 func createRsaKey(w http.ResponseWriter, r *http.Request) {
@@ -77,11 +128,14 @@ func createRsaKey(w http.ResponseWriter, r *http.Request) {
 func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/", index)
+	router.HandleFunc("/uuid", getUUID)
 	router.HandleFunc("/sha256/{data}", sha256Api).Methods("GET").Queries("encoding", "{encoding}")
 	router.HandleFunc("/sha512/{data}", sha512Api)
 	router.HandleFunc("/symmetrickey", createSymmKey).Queries("bits", "{bits}")
+	router.HandleFunc("/listkeys", listSymmKeys)
+	router.HandleFunc("/aes/encrypt/{plaintext}", aesEncrypt).Queries("keyid", "{keyid}")
+	router.HandleFunc("/aes/decrypt/{ciphertext}", aesDecrypt).Queries("keyid", "{keyid}")
 	router.HandleFunc("/rsa/keys", createRsaKey)
-	router.HandleFunc("/aes/encrypt/{plaintext}", createRsaKey).Queries("key", "{key}")
 	// router.HandleFunc("/rsa/encrypt/{plaintext}", rsaEncrypt)
 	// router.HandleFunc("/rsa/decrypt/{ciphertext}", rsaDecrypt)
 	// router.HandleFunc("/rsa/sign/{message}", rsaSign)
